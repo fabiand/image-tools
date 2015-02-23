48091addc02edef01438d8be24fcc4d4dde1b2cd
Fabian Deutsch <fabiand@fedoraproject.org>
2015-02-23 15:58:41 +0100

    Force TCG for sparsification
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- build.mk
+++ build.mk
@@ -15,11 +15,13 @@ noop:
 
 # Direct for virt-sparsify: http://libguestfs.org/guestfs.3.html#backend
 export LIBGUESTFS_BACKEND=direct
+# Workaround nest problem: https://bugzilla.redhat.com/show_bug.cgi?id=1195278
+export LIBGUESTFS_BACKEND_SETTINGS=force_tcg
 %.qcow2: %.ks
 	bash $(mkfile_dir)/anaconda_install $(DISTRO) $(RELEASEVER) $< $@ $(DISK_SIZE)
-	#-virt-sparsify --in-place $@ 
-	#  || virt-sparsify --check-tmpdir=continue --compress $@ $@.sparse && mv -v $@.sparse $@ 
-	#  || virt-sparsify --compress $@ $@.sparse && mv -v $@.sparse $@
+	-virt-sparsify --in-place $@ \
+	  || virt-sparsify --check-tmpdir=continue --compress $@ $@.sparse && mv -v $@.sparse $@ \
+	  || virt-sparsify --compress $@ $@.sparse && mv -v $@.sparse $@
 
 %.raw: %.qcow2
 	qemu-img convert -p -S 1M -O raw $< $@
